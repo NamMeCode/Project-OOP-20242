d24a94d864038d1e04c9042bf72facd095cc10b5
Nam <namcao2005@gmail.com>
2025-03-01 10:06:27 +0700

    complete assignment 2.2.2

---
+++ Nam_OOP_20242/src/The_very_first_Java_program_2_2/FirstDialogJava.java
@@ -0,0 +1,9 @@
+package The_very_first_Java_program_2_2;
+import javax.swing.JOptionPane;
+
+public class FirstDialogJava {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
