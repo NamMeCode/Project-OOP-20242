323c6c837078018c1dd0b51fc4bae21768976bab
Nam <namcao2005@gmail.com>
2025-03-01 10:13:50 +0700

    complete assignment 2.2.6

--- Nam_OOP_20242/src/The_very_first_Java_program_2_2/FirstDegreeEquation1Var.java
+++ Nam_OOP_20242/src/The_very_first_Java_program_2_2/FirstDegreeEquation1Var.java
@@ -7,7 +7,7 @@ public class FirstDegreeEquation1Var {
         double a=Double.parseDouble(JOptionPane.showInputDialog(null, "Please enter the first coefficient: ", "Input the first coefficient", JOptionPane.INFORMATION_MESSAGE));
         double b=Double.parseDouble(JOptionPane.showInputDialog(null, "Please enter the second coefficient: ", "Input the second coefficient", JOptionPane.INFORMATION_MESSAGE));
         if(a==0&&b!=0) JOptionPane.showMessageDialog(null, "The equation bears no solution");
-        else if(a==0&&b==0) JOptionPane.showMessageDialog(null, "The equation has infinitely many solutions");
+        else if(a==0) JOptionPane.showMessageDialog(null, "The equation has infinitely many solutions");
         else {
             double sol=-b/a;
             JOptionPane.showMessageDialog(null, "The solution of the equation is: " + sol);
