907913438f2793ffe75808885c16b3c7c541f98f
Nam <namcao2005@gmail.com>
2025-03-01 09:32:37 +0700

    complete assignment 2.2.1

---
+++ Nam_OOP_20242/src/The_very_first_Java_program_2_2/HelloWorld.java
@@ -0,0 +1,8 @@
+package The_very_first_Java_program_2_2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t World!");
+    }
+}
