18763c709c52d73d8db16547513b26559a833880
NLightVN <hung24378un@gmail.com>
2025-04-21 09:11:01 +0700

    Something new

---
+++ src/card/ListOfCards.java
@@ -0,0 +1,78 @@
+package card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ListOfCards {
+    private ArrayList<Card> cardList=new ArrayList<>();
+    private int size=0;
+    public int getSize()
+    {
+        return size;
+    }
+    public ArrayList<Card> getCardList()
+    {
+        return cardList;
+    }
+    public void shuffle() {
+        Collections.shuffle(cardList);
+    }
+
+    public void sort() {
+        cardList.sort(Comparator.comparing(Card::getRank).thenComparing(Card::getSuit));
+    }
+    public ListOfCards()
+    {
+        ;
+    }
+    public ListOfCards(ArrayList<Card> cardList)
+    {
+        this.cardList=cardList;
+        this.size=cardList.size();
+    }
+    public void addCard(Card card) {
+        cardList.add(card);
+        size++;
+    }
+
+    public boolean removeCard(Card card) {
+        size--;
+        return cardList.remove(card);
+
+    }
+    public Card getCardAt(int index) {
+        if (index >= 0 && index < size) {
+            return cardList.get(index);
+        }
+        return null;
+    }
+    public ListOfCards cardsSelected()
+    {
+        ArrayList <Card> cardsSelected=new ArrayList<>();
+        for (Card card : cardList) {
+            if (card.isSelected()) {
+                cardsSelected.add(card);
+
+            }
+        }
+        return new ListOfCards(cardsSelected);
+    }
+    public ListOfCards cardsNotSelected()
+    {
+        ArrayList <Card> cardsNotSelected=new ArrayList<>();
+        for (Card card : cardList) {
+            if (!card.isSelected()) {
+                cardsNotSelected.add(card);
+
+            }
+        }
+        return new ListOfCards(cardsNotSelected);
+    }
+    public void replacedBy(ListOfCards newList)
+    {
+        cardList=newList.getCardList();
+        size=newList.size;
+    }
+
+}
