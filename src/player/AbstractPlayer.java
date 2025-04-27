0fa1baade6399e828b0dd376ce5017544ced3416
NLightVN <hung24378un@gmail.com>
2025-04-28 00:03:19 +0700

    Something old

---
+++ src/player/AbstractPlayer.java
@@ -0,0 +1,61 @@
+package player;
+import card.Card;
+import card.ListOfCards;
+import rule.GameRule;
+import rule.ThirteenSRuleRemake;
+
+public abstract class AbstractPlayer {
+    private ListOfCards cardsOnHand= new ListOfCards();
+    private String gameType;
+    GameRule rule;
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+    public void setRule(String rule) {
+        switch (rule) {
+            case "ThirteenS":
+                this.rule=new ThirteenSRuleRemake();
+        }
+    }
+
+    public void selectCard(int index) {
+        Card cardChosen= cardsOnHand.getCardAt(index);
+        if (cardChosen!=null) {
+            cardChosen.setSelected(true);
+
+        }
+
+
+    }
+    public void unSelectCard(int index) {
+        Card cardChosen= cardsOnHand.getCardAt(index);
+        if (cardChosen!=null) {
+            cardChosen.setSelected(false);
+        }
+    }
+    public void addCard(Card card) {
+        cardsOnHand.addCard(card);
+    }
+    public void removeCard(Card card) {
+        cardsOnHand.removeCard(card);
+    }
+    public boolean playCards(ListOfCards cardsOnTable) {
+        ListOfCards cardsPlayed = cardsOnHand.cardsSelected();
+        if (rule.checkValidPlay(cardsPlayed,cardsOnTable))
+        {
+            cardsOnTable.replacedBy(cardsPlayed);
+            cardsOnHand.replacedBy(cardsOnHand.cardsNotSelected());
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+}
